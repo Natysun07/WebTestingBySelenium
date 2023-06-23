53a0b3d24ff5f60ee8840b2eff490950da4db415
Natalia <natysun07@rambler.ru>
2023-06-23 16:33:45 +0300

    Initial commit

---
+++ src/test/java/ru/netology/selenium/CallbackTest.java
@@ -0,0 +1,28 @@
+package ru.netology.selenium;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class CallbackTest {
+    private WebDriver driver;
+
+    @BeforeAll
+    static void setUpAll() {
+        System.setProperty("webdriver.chrome.driver", "driver\\win\\chromedriver.exe");
+    }
+
+    @BeforeEach
+    void setUp() {driver = new ChromeDriver();
+    }
+
+    @AfterEach
+    void tearDown() {
+        driver.quit();
+        driver = null;
+    }
+
+
+}
